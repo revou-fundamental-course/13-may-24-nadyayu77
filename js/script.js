58cf4eb078eb8d31541c3bd14edb3a57df3f26ee
nadyayu77 <nadyayuparamitha77@gmail.com>
2024-05-23 00:58:02 +0700

    Update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 // Ini JS
+let iskelilingvisible = false; //Keliling is Hide
+function hideOrShow() {
+    if (iskelilingvisible == false) {
+        iskelilingvisible = true;
+        document.getElementById('luas-section').style.display = 'none';
+        document.getElementById('keliling-section').style.display = 'block';
+    } else {
+        iskelilingvisible = false;
+        document.getElementById('luas-section').style.display = 'block';
+        document.getElementById('keliling-section').style.display = 'hide';
+}
+    }
